3d05cd80f2a832d8dd7e806bf9b48f0f2b058719
Chase Nickel <nickel5@outlook.com>
2024-08-09 18:17:20 -0500

    Split display

---
+++ src/Display.zig
@@ -0,0 +1,44 @@
+const Self = @This();
+
+protocol: void, // TODO: What is this
+host: []const u8,
+number: u16,
+screen: void, // TODO: What is this
+
+/// Get the display environment variable
+/// ":0" if not defined
+/// Windows sometimes needs to allocate because it's a little silly
+pub fn getEnvVar(alloc: std.mem.Allocator) ![]const u8 {
+    return switch (@import("builtin").os.tag) {
+        .windows => std.process.getEnvVarOwned(alloc, "DISPLAY") catch |err| switch (err) {
+            error.EnvironmentVariableNotFound => ":0",
+            else => err,
+        },
+        else => std.posix.getenv("DISPLAY") orelse ":0",
+    };
+}
+
+const ParseError = error{
+    MalformedInput,
+    MissingDisplayNumber,
+    InvalidDisplayNumber,
+    InvalidScreenNumber,
+};
+/// Parse a display variable into a Display struct
+/// Format: [PROTOCOL/]HOST:DISPLAYNUM[.SCREEN]
+/// ex:
+///     localhost:10.0
+///     -> host: localhost
+///     -> displaynum: 10
+///     -> screen: 0
+pub fn parse(display: []const u8) ParseError!Self {
+    _ = display; // autofix
+    unreachable; // unimplemented
+}
+
+pub fn fromEnvVar(alloc: std.mem.Allocator) !Self {
+    const display_string = try getEnvVar(alloc);
+    return parse(display_string);
+}
+
+const std = @import("std");
--- src/root.zig
+++ src/root.zig
@@ -254,48 +254,7 @@ pub const Screen = struct {
 
 const opcode = @import("opcode.zig");
 
-pub const Display = struct {
-    protocol: void, // TODO: What is this
-    host: []const u8,
-    number: u16,
-    screen: void, // TODO: What is this
-
-    /// Get the display environment variable
-    /// ":0" if not defined
-    /// Windows sometimes needs to allocate because it's a little silly
-    pub fn getEnvVar(alloc: std.mem.Allocator) ![]const u8 {
-        return switch (builtin.os.tag) {
-            .windows => std.process.getEnvVarOwned(alloc, "DISPLAY") catch |err| switch (err) {
-                error.EnvironmentVariableNotFound => ":0",
-                else => err,
-            },
-            else => std.posix.getenv("DISPLAY") orelse ":0",
-        };
-    }
-
-    const ParseError = error{
-        MalformedInput,
-        MissingDisplayNumber,
-        InvalidDisplayNumber,
-        InvalidScreenNumber,
-    };
-    /// Parse a display variable into a Display struct
-    /// Format: [PROTOCOL/]HOST:DISPLAYNUM[.SCREEN]
-    /// ex:
-    ///     localhost:10.0
-    ///     -> host: localhost
-    ///     -> displaynum: 10
-    ///     -> screen: 0
-    pub fn parse(display: []const u8) ParseError!Display {
-        _ = display; // autofix
-        unreachable; // unimplemented
-    }
-
-    pub fn fromEnvVar(alloc: std.mem.Allocator) !Display {
-        const display_string = try getEnvVar(alloc);
-        return parse(display_string);
-    }
-};
+pub const Display = @import("Display.zig");
 
 pub const ConnectResponse = struct {
     const BitmapInfo = struct {
