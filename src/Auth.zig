4dfe250af72c3a6c16f81498827976d3a4c68bb7
Chase Nickel <nickel5@outlook.com>
2024-08-12 02:46:25 -0500

    Auth file reading

--- src/Auth.zig
+++ src/Auth.zig
@@ -32,5 +32,58 @@ fn getAuthFilePathPosix(alloc: std.mem.Allocator) !?[]const u8 {
     return null;
 }
 
+fn readAuthFile(alloc: std.mem.Allocator, path: []const u8) []const AuthFileEntry {
+    var entries = std.ArrayList(AuthFileEntry).init(alloc);
+    const auth_file = try std.fs.cwd().openFile(path, .{});
+    defer auth_file.close();
+    const auth_file_reader = auth_file.reader();
+    const reader_buffered = std.io.bufferedReader(auth_file_reader);
+    const reader = reader_buffered.reader();
+
+    while (true) {
+        var entry: AuthFileEntry = undefined;
+        entry.family = try reader.readInt(u16, .big);
+
+        const host_address_len = try reader.readInt(u16, .big);
+        entry.host_address = alloc.alloc(u8, host_address_len);
+        try reader.readAll(entry.host_address);
+
+        const display_number_len = try reader.readInt(u16, .big);
+        entry.display_number = alloc.alloc(u8, display_number_len);
+        try reader.readAll(entry.display_number);
+
+        const auth_name_len = try reader.readInt(u16, .big);
+        entry.auth_name = alloc.alloc(u8, auth_name_len);
+        try reader.readAll(entry.auth_name);
+
+        const auth_data_len = try reader.readInt(u16, .big);
+        entry.auth_data = alloc.alloc(u8, auth_data_len);
+        try reader.readAll(entry.auth_data);
+
+        try entries.append(entry);
+    }
+
+    return entries.toOwnedSlice();
+}
+
+fn deinitAuthFileList(alloc: std.mem.Allocator, entries: []const AuthFileEntry) void {
+    for (entries) |entry| {
+        alloc.free(entry.host_address);
+        alloc.free(entry.display_number);
+        alloc.free(entry.auth_name);
+        alloc.free(entry.auth_data);
+    }
+    alloc.free(entries);
+}
+
+const AuthFileEntry = struct {
+    family: u16,
+    host_address: []u8,
+    display_number: []u8,
+    auth_name: []u8,
+    auth_data: []u8,
+};
+
+const common = @import("common.zig");
 const builtin = @import("builtin");
 const std = @import("std");
